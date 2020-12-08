c573010ad096cb44d49c460cf087ee3e16fef672
Snehal Kashetti <kashettisnehal@gmail.com>
2020-12-08 20:56:40 +0530

    initial commit

---
+++ src/main/java/com/firstProj/eCommerce/App.java
@@ -0,0 +1,13 @@
+package com.firstProj.eCommerce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
